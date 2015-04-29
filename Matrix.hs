9c0861534f42a232e636741aba5da34549ddf2cb
Mitchell Riley <mitchell.v.riley@gmail.com>
2015-04-30 00:13:11 +1000

    generalise matrix a bit

--- Matrix.hs
+++ Matrix.hs
@@ -10,10 +10,10 @@ solveAxb (a11, a12,
      (-a21 * b1 + a11 * b2) / det)
   where det = a11 * a22 - a12 * a21
 
-a :: (Double, Double, Double, Double)
+a :: Fractional t => (t, t, t, t)
 a = (64919121,   -159018721,
      41869520.5, -102558961)
 
-b :: (Double, Double)
+b :: Fractional t => (t, t)
 b = (1,
      0)
